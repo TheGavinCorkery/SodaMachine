17ce3ff3b8e1379b9073b76ad847681233023e5f
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-10-22 15:31:18 -0500

    SimulationMainMenu Method Complete
    SimulationMainMenu Method Complete

--- UserInterface.cs
+++ UserInterface.cs
@@ -6,74 +6,94 @@ using System.Threading.Tasks;
 
 namespace SodaMachine
 {
-    static class UserInterface
+    public static class UserInterface
     {
-        static void SimulationMainMenu()
+        public static string SimulationMainMenu()
         {
             //Main menu prompting user to choose an option
+            bool invalidUserSelection = true;
+            string userInput;
+            while (invalidUserSelection)
+            {
+                Console.WriteLine("Simulation Menu\n"
+                                  + "Press -1- to begin transaction\n"
+                                  + "Press -2- to check wallet for coins\n"
+                                  + "Press -3- to check backpack for cans\n"
+                                  + "Press -4- to terminate simulation");
+                userInput = Console.ReadLine();
+                invalidUserSelection = ValidateMainMenu(userInput);
+                if(invalidUserSelection == true)
+                {
+                    return userInput;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input Try Again.");
+                }
+            }
         }
 
-        static void ValidateMainMenu(string userInput)
+        public static bool ValidateMainMenu(string userInput)
         {
             //Validation function that checks if 'user_input' argument is an int 1-4.
         }
 
-        static void DisplayCustomerWalletInfo(double totalValue)
+        public static void DisplayCustomerWalletInfo(double totalValue)
         {
             //Display what is remaining in Customer Wallet
         }
 
-        static void DisplayWelcome()
+        public static void DisplayWelcome()
         {
             //Asks user if they would like to make a purchase
         }
 
-        static void OutputText(string text)
+        public static void OutputText(string text)
         {
             Console.WriteLine(text);
         }
 
-        static void ClearConsole()
+        public static void ClearConsole()
         {
             //Clears console
         }
 
-        static void ContinuePrompt(string text)
+        public static void ContinuePrompt(string text)
         {
             //validates a yes or no response
         }
 
-        static void SodaSelection(List<Can> inventory)
+        public static void SodaSelection(List<Can> inventory)
         {
             //Displays the inventory of sodas and allows for selection
         }
 
-        static void ValidateCanChoice(string selection)
+        public static void ValidateCanChoice(string selection)
         {
             //Validates User Can selection
         }
 
-        static void GetUniqueCanNames(List<Can> inventory)
+        public static void GetUniqueCanNames(List<Can> inventory)
         {
             //Loops through inventory to create a list of all distinct types of sodas available.
         }
 
-        static void DisplayCanCost(Can selectedCan)
+        public static void DisplayCanCost(Can selectedCan)
         {
             //Displays the cost of the can as the user puts coins in.
         }
 
-        static void DisplayPaymentValue(Coin customerPayment)
+        public static void DisplayPaymentValue(Coin customerPayment)
         {
             //Displays the value of selected coins as customer is choosing coins to deposit
         }
 
-        static void CoinSelection()
+        public static void CoinSelection()
         {
             //Prompts user to choose which coins to deposit and passes their selection in validate_coin_selection
         }
 
-        static void EndMessage(string sodaName, double changeAmount)
+        public static void EndMessage(string sodaName, double changeAmount)
         {
             //Closing message for the user.
         }
