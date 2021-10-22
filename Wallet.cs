bc9d2d6c475841695c284181f1448a622cb3dc53
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-10-22 12:08:22 -0500

    Wallet Class completed with coins added and value calculated
    Wallet Class completed with coins added and value calculated

--- Wallet.cs
+++ Wallet.cs
@@ -9,10 +9,37 @@ namespace SodaMachine
     class Wallet
     {
         List<Coin> Coins;
+        double totalValue;
 
-        public void FillRegister()
+        public void FillWallet()
         {
+            int quartersNeeded = 9;
+            int dimesNeeded = 15;
+            int nicklesNeeded = 20;
+            int pennyNeeded = 25;
 
+            int[] coinsNeeded = { quartersNeeded, dimesNeeded, nicklesNeeded, pennyNeeded };
+
+            for (int i = 0; i < 25; i++)
+            {
+                if(i < quartersNeeded)
+                {
+                    Coins.Add(new Quarter());
+                    totalValue += .25;
+                }
+                if (i < dimesNeeded)
+                {
+                    Coins.Add(new Dime());
+                    totalValue += .10;
+                }
+                if (i < nicklesNeeded)
+                {
+                    Coins.Add(new Nickle());
+                    totalValue += .05;
+                }
+                Coins.Add(new Penny());
+                totalValue += .01;
+            }
         }
     }
 }
