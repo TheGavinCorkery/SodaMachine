f27230d4302bec41dea29e54402f2f37031479e2
Gavin Corkery <GavinCorkery@hotmail.com>
2021-10-22 13:31:06 -0400

    Completed FillRegister and Inventory Methods

--- SodaMachine.cs
+++ SodaMachine.cs
@@ -8,5 +8,39 @@ namespace SodaMachine
 {
     class SodaMachine
     {
+        List<Coin> register;
+        List<Can> inventory;
+
+        public SodaMachine()
+        {
+            FillInventory();
+            FillRegister();
+        }
+
+        public void FillRegister()
+        {
+            for (int x = 0; x < 49; x++)
+            {
+                if (x < 20)
+                {
+                    register.Add(new Quarter());
+                    register.Add(new Nickle());
+                }
+                if (x < 10)
+                {
+                    register.Add(new Dime());
+                }
+                register.Add(new Penny());
+            }
+        }
+        public void FillInventory()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                inventory.Add(new RootBeer());
+                inventory.Add(new Cola());
+                inventory.Add(new Orange());
+            }
+        }
     }
 }
